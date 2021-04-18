042609b5325d0a1cc21f46608e16938ed051001e
etamar <etamar.levy@gmail.com>
2021-04-17 21:03:12 -0400

    commit tree 7c8c625833cd4fb405a96f495b452d3f24f4b264

--- main.js
+++ main.js
@@ -2,7 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+// global variables
+const like = document.getElementsByClassName("like-glyph");
+const errorMessage = document.getElementById('modal');
+errorMessage.setAttribute('class', 'hidden')
+
+
+
 // Your JavaScript code goes here!
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall()
+  .then(() => {
+    if ( heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.className = "activated-heart";
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+})
+.catch((error) => {
+  errorMessage.removeAttribute("class")
+  let h2 = errorMessage.querySelector("h2")
+  h2.innerText = "Error!, Random server error. Try again."
+  setTimeout(() => {
+    errorMessage.setAttribute('class', 'hidden')
+  }, 3000)
+})
+}
+
+
+
+for (i = 0; i < like.length; i++) {
+  like[i].addEventListener('click', likeCallback);
+}
+
+
+
+
 
 
 
